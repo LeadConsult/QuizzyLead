0a1f25c4897afe4d31ae0dbba8cebd0d3816c2a7
LeadConsult <106965102+LeadConsult@users.noreply.github.com>
2023-09-08 00:30:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def job():
         print(f"Job run at {now_gmt1}")
 
         # # Add error handling for create_and_post_tweet
-        # create_and_post_tweet(client_v1, client_v2)
+        create_and_post_tweet(client_v1, client_v2)
     except Exception as e:
         print(f"Error in job: {e}")
 
@@ -42,7 +42,6 @@ def api():
     try:
         # Call the job function
         job()        
-        create_and_post_tweet(client_v1, client_v2)
         print("Script executed from job")
         return jsonify({"message": "Script executed successfully"})
     except Exception as e:
