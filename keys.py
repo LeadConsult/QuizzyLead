35dc8dee76f6a2451789abef7452b7b2ccdb26d3
LeadConsult <106965102+LeadConsult@users.noreply.github.com>
2023-09-07 02:32:17 +0100

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,6 +1,8 @@
-api_key = "keWY91QJEOBrFbs7BrNhfdVrd"
-api_secret = "8blzGn0ngUhJ2XqTLB5DKkKCVCTSfyt8H3WQj7lEoPPVvwZX1a"
-access_token = "522665405-TDjSA6cabdnFsG8ntotP5tRyoziqUTgRgeAR5AJi"
-access_token_secret = "WP5pj3DwJH4VYDdtN21OhgfRgd48kd184WUFpzSmDsGfR"
-client_id = "YmlKNTZIcWJTNjNwTW03N1VnOTM6MTpjaQ"
-client_secret = "ruMrrKZxp8HE3oJZAzMhA-zD1-930117WPzeAtHDBk8Vu_GsCW"
+import os
+
+api_key = os.environ.get('api_key')
+api_secret = os.environ.get('api_secret')
+access_token = os.environ.get('access_token')
+access_token_secret = os.environ.get('access_token_secret')
+client_id = os.environ.get('client_id')
+client_secret = os.environ.get('client_secret')
